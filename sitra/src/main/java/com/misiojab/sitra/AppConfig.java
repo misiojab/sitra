9ebb75f9b68b601be79272f6ee7eff2e4d5844c7
misiojab <34603206+misiojab@users.noreply.github.com>
2022-01-25 04:06:25 +0100

    Delete sitra directory

--- sitra/src/main/java/com/misiojab/sitra/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.misiojab.sitra;
-
-public class AppConfig {
-
-    public static final String DATABASE_DRIVER = "com.mysql.jdbc.Driver";
-    public static final String DATABASE_URI = "jdbc:mysql://localhost:3306";
-
-    private AppConfig() {}
-}
--- sitra/src/main/java/com/misiojab/sitra/Utils/ThrowException.java
+++
@@ -1,8 +0,0 @@
-package com.misiojab.sitra.Utils;
-
-@FunctionalInterface
-public interface ThrowException<V, R, E extends Exception> {
-
-    R execute(V value) throws E;
-
-}
